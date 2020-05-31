4f1cf45313d480cceeac0a3b599c2cc37fc64f45
Friso H <42006903+Toukie@users.noreply.github.com>
2020-05-31 09:26:02 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
 questions = [
     // friesland vs nederland
     {
-        "question": "update 1 Friesland moet onafhankelijk worden.",
+        "question": "update 2 Friesland moet onafhankelijk worden.",
         "effect": {
             "FRL": 10,
             "REG": 5,
